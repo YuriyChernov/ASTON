03c68d4c09c738365f4bd048eedd2234c0b102f7
Yuriy Chernov <Chernov.y.12@gmail.com>
2024-01-13 12:35:50 +0300

    Add methods

--- Lesson_2/src/main/java/aston/Main.java
+++ Lesson_2/src/main/java/aston/Main.java
@@ -5,8 +5,53 @@ public class Main {
         System.out.println("Orange\nBanana\nApple");
     }
 
+    public void checkSumSign() {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public void printColor() {
+        int value = 3;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public void compareNumbers() {
+        int a = 25;
+        int b = 50;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
     public static void main(String[] args) {
         Main main = new Main();
+        System.out.println("Задание 1");
         main.printThreeWords();
+        System.out.println();
+        System.out.println("Задания 2");
+        main.checkSumSign();
+        System.out.println();
+        System.out.println("Задание 3");
+        main.printColor();
+        System.out.println();
+        System.out.println("Задание 4");
+        main.compareNumbers();
     }
 }
