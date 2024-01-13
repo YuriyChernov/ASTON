1e5ddba269fc8e2f198ca434dd863d6d8643b4d8
Yuriy Chernov <Chernov.y.12@gmail.com>
2024-01-13 11:51:50 +0300

    Initial commit

---
+++ src/main/java/aston/Main.java
@@ -0,0 +1,12 @@
+package aston;
+
+public class Main {
+    public void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.printThreeWords();
+    }
+}
